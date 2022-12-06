fce5c4489dfbe37a63d1a4f19bba3cca31d315e8
Selin Ayyildiz <xcadyix@gmail.com>
2022-12-06 16:11:26 -0500

    commit tree e48dd7251541ffb5a587ac300252d88a1182245b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+function submitData (name, email) {
+
+    const formData = {
+        name: name,
+        email: email
+    }
+
+ const configurationObject = {
+    method: "POST",
+    headers: {
+    "Content-Type" : "application/json",
+    "Accept" : "application/json",
+    },
+    body: JSON.stringify(formData)
+}
+   
+return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    })
+    .then(function(object) {
+    })
+}
